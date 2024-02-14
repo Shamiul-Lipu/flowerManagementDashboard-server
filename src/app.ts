3b584ccfd91cfd60c75edaea5bbfedce31ed7f7c
Shamiul-Lipu <shamiullipu@gmail.com>
2024-02-14 07:24:50 +0600

    initializing flowerManagementDashboard-server

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 const app: Application = express();
 
 // parser
-app.use(express.json());
+// app.use(express.json());
 app.use(cookieParser());
 
 app.use(
